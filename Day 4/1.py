d78c2735968e26fabebeb61b653b7d61088978d0
Jaka Kovač <jakakovac@duck.com>
2022-12-04 07:36:07 +0100

    Day 4

---
+++ Day 4/1.py
@@ -0,0 +1,20 @@
+with open("input.txt", 'r') as f:
+    f = f.read().split()
+    f = [[j.split("-") for j in i.split(",")] for i in f]
+
+    g = []
+    for i in f:
+        if int(i[0][0]) <= int(i[1][0]):
+            g.append(i)
+        else:
+            g.append(i[::-1])
+
+    contains = 0
+    for i in g:
+        if (int(i[0][1]) >= int(i[1][1]) and int(i[0][1]) >= int(i[1][0])) or int(i[0][0]) == int(i[1][0]):
+            contains += 1
+        
+    
+
+
+    print(contains)
